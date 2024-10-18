89b75cda46ea146d5cbbd1ab58504e231e28aee6
Christian Vaughn <me@christianvaughn.net>
2024-10-17 18:38:13 -0700

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -33,18 +33,18 @@ const config = {
 		prerender: {
 			entries: [
 				'/blog/api/posts/page/[page]',
-				// '/blog/category/[category]/page/',
+				'/blog/category/[category]/page/',
 				'/blog/category/page/',
 				'/blog/page/',
-				// '/blog/page/[page]',
+				'/blog/page/[page]',
 				'/blog/api/posts.json',
 				'/blog/api/posts/count',
 				'/blog/api/posts/page/[page]',
 				'/blog/api/rss.xml',
 				'/legal/privacy/',
-				'/legal/terms/'
-				// '/blog/category/[category]/page/[page]',
-				// '/blog/category/page/[page]'
+				'/legal/terms/',
+				'/blog/category/[category]/page/[page]',
+				'/blog/category/page/[page]'
 			]
 		},
 		version: {
